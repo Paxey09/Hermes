481dc89f7e5f991a21cdd14b1a12315a29aa3091
Paxey Lyrates <paxeylyrates@gmail.com>
2026-04-23 09:35:21 +0800

    feat: add privacy policy page

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({
 	verify: (req, res, buf) => {
 		req.rawBody = buf;
